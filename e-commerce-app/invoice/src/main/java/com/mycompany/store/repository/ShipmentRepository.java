9a27a40365baf851ccb54b50cc4e3c3d932cfb0f
deepu105 <d4udts@gmail.com>
2019-12-08 13:40:11 +0100

    invoice application generated using JDL

---
+++ e-commerce-app/invoice/src/main/java/com/mycompany/store/repository/ShipmentRepository.java
@@ -0,0 +1,14 @@
+package com.mycompany.store.repository;
+import com.mycompany.store.domain.Shipment;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Shipment entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+}
